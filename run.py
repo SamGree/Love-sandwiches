92bc70ad3d35456fd2a5dcbbcb50ccbb5132ef2e
Sam Green <sam.green7777@yahoo.com>
2024-06-05 09:15:54 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -17,6 +17,9 @@ SHEET = GSPREAD_CLIENT.open('my_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -53,11 +56,11 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):    
+def update_sales_worksheet(data):
     """
-    update sales worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
-    print("updating sales woksheet...\n")
+    print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
@@ -74,7 +77,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
 def main():
@@ -84,7 +93,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
-print("welcome to my danswiches Data Automation")
+print("Welcome to Love Sandwiches Data Automation")
 main()
